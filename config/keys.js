f953dfb061949411862a4c78b884d04588b1f3d9
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-05-01 15:27:04 +0530

    adding routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb://amandeeprikhi:aman131997@ds119052.mlab.com:19052/devconnecter'
+};
--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDb CONNECTED !!!!"))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello world!!')
 });
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000;
 
